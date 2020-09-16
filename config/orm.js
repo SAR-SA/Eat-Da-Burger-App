bcb969ba772f3f0935a88f6b108829821cfa7610
SAR-SA <shawn.a.rather@gmail.com>
2020-09-15 22:52:36 -0400

    Skeleton created

---
+++ config/orm.js
@@ -0,0 +1,13 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+function selectAll();
+
+function insertOne();
+
+function updateOne();
+
+function deleteOne();
+
+// Export the orm object for the model
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('The Mountin');
+INSERT INTO burgers (burger_name) VALUES ('Molten');
+INSERT INTO burgers (burger_name) VALUES ('Plainsville Burger');
