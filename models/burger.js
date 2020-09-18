d78b2dc4a294f8154a871c29bb85eb3732eef111
SAR-SA <shawn.a.rather@gmail.com>
2020-09-17 22:37:20 -0400

    added controllers, models, public. Fixed Main.

--- models/burger.js
+++ models/burger.js
@@ -25,5 +25,5 @@ var burger = {
     }
   };
   
-  // Export the database functions for the controller (catsController.js).
+  // Export the database functions for the controller (Burgers_controller.js).
   module.exports = burger;
